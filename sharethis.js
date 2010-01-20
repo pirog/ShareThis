93580e05a055151c0069812b76b7c23a10bd5249
Rob Loach <robloach@gmail.com>
2010-01-20 22:51:24 +0000

    #621802 by quicksketch: JavaScript errors.

--- sharethis.js
+++ sharethis.js
@@ -7,15 +7,19 @@ Drupal.behaviors.sharethis = function(context) {
   // Process each element.
   if (Drupal.settings.sharethis) {
     jQuery.each(Drupal.settings.sharethis, function(id, options) {
+      var element = document.getElementById(id);
+      if (!element) {
+        return;
+      }
       var shared_object = SHARETHIS.addEntry({
         title: options['title'],
         url: options['url']
       });
       if (options['chicklet'] || false) {
-        shared_object.attachChicklet(options['chicklet'], document.getElementById(id));
+        shared_object.attachChicklet(options['chicklet'], element);
       }
       else {
-        shared_object.attachButton(document.getElementById(id), {button: false});
+        shared_object.attachButton(element, {button: false});
       }
       $('#' + id).click(function() { return false; });
       delete(Drupal.settings.sharethis[id]);
